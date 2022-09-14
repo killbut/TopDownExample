d4758ca1c47749601244c71af6f0092453997074
killbut <72091291+killbut@users.noreply.github.com>
2022-09-14 23:39:19 +0700

    Add movement and shoot

---
+++ Assets/Scripts/PlayerFire.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerFire : MonoBehaviour
+{
+    [SerializeField] private GameObject _bullet;
+    [SerializeField] private float _bulletSpeed;
+    [SerializeField] private Transform _firePositionTransform;
+    
+    protected void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Player shoot");
+            var bullet=Instantiate(_bullet, _firePositionTransform.position, _firePositionTransform.rotation);
+            bullet.GetComponent<Rigidbody2D>().AddForce(_firePositionTransform.up*_bulletSpeed,ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private Rigidbody2D _rigidbody2D;
+    private Vector2 _movementVector;
+    private Camera _camera;
+    private Vector2 _mousePosition;
+
+    protected void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _camera = Camera.main;
+    }
+
+    protected void Update()
+    {
+        GetInputPosition();
+        _mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    protected void FixedUpdate()
+    {
+        MovePlayer();
+        RotatePlayer();
+    }
+
+    private void GetInputPosition()
+    {
+        _movementVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    private void MovePlayer()
+    {
+        _rigidbody2D.MovePosition(_rigidbody2D.position + _movementVector * (_speed * Time.fixedDeltaTime));
+    }
+
+    private void RotatePlayer()
+    {
+        var lookingDirection = _mousePosition - _rigidbody2D.position;
+        float angle = Mathf.Atan2(lookingDirection.y, lookingDirection.x) * Mathf.Rad2Deg - 90f;
+        _rigidbody2D.rotation = angle;
+    }
+}
